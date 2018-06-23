849847c4aa3eea8c079276c39f8f6e7054ee01e6
coder-pig <2744004266@qq.com>
2018-06-23 14:23:22 +0800

    moive

---
+++ Assets/MovieUrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class MovieUrl : MonoBehaviour {
+
+     string path = "C:\\Users\\Administrator.USER-20180109JC\\Desktop\\Cinematic Enus.ogv";
+      MovieTexture movie;
+    public RawImage movierawimage;
+     AudioSource movieaudio;
+    void Start()
+    {
+        movieaudio = GetComponent<AudioSource>();
+         WWW www = new WWW("file:///" + path);
+       
+        movie = www.movie;
+        movierawimage.texture = movie;
+        movie.loop = false;
+        movieaudio.clip = movie.audioClip;
+
+
+        movie.Play();
+        movieaudio.Play();
+    }
+
+
+
+}
